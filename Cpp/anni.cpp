3567ce3af85660cf800fc30370f00b85e6877c41
cxxu <838808930@qq.com>
2024-09-15 22:46:07 +0800

    general update project 09/15/2024 22:46:07

--- Cpp/anni.cpp
+++ Cpp/anni.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-
-void clearScreen() {
-    std::cout << "\033[2J\033[1;1H"; // ANSI escape codes to clear screen and move cursor to (1,1)
-}
-
-int main() {
-    for (int i = 0; i < 10; ++i) {
-        clearScreen();
-        std::cout << "Frame " << i + 1 << std::endl;
-        std::cout << "  O  " << std::endl;
-        std::cout << " /|\\ " << std::endl;
-        std::cout << " / \\ " << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // delay for 500 milliseconds
-    }
-
-    return 0;
+using namespace std;
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << 120 * x + 0.5 * y * 120 + 0.6 * z * 120 << endl;
 }
