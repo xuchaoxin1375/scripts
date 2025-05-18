e755cc6639f0353161cfc2eb4e507eb0bcffbee3
cxxu <838808930@qq.com>
2025-05-18 10:39:58 +0800

    temp commit

--- PS/Git/Git.psm1
+++ PS/Git/Git.psm1
@@ -54,9 +54,63 @@ $GithubMirrors = $GithubMirrors | ForEach-Object { $_.trim('/') } | Sort-Object 
 $GithubMirrors = $GithubMirrors | Select-Object -Unique # Get-Unique #移除重复条目(注意get-Unique要求被处理数组是有序的,否则无效,可以用select -unique更通用)
 
 
+
 function Test-LinksLinearly
 {
- 
+    <# 
+    .SYNOPSIS
+    线性地(串行地)测试链接是否能够在指定时间内响应,为powershell5 设计
+    .NOTES
+    链接数量多的话会造成测试时间很长,尽量使用并行方案(pwsh7),或者考虑设置小的$timeoutSec=1
+    #>
+    [cmdletbinding(DefaultParameterSetName = 'First')]
+    param (
+        $Mirrors = $GithubMirrors,
+        $TimeOutSec = 6,
+        [parameter(ParameterSetName = 'First')]
+        $First = 5,
+        [parameter(ParameterSetName = 'All')]
+        [Alias('Full')]
+        [switch]
+        $All
+    )
+    $availableMirrors = @()
+    Write-Debug "Test links linearly...🎈" -Debug
+    foreach ($mirror in $Mirrors)
+    {
+        # $Mirrors | ForEach-Object {
+        # $mirror = $_
+
+        # Write-Verbose "Testing $mirror..."
+        if (Test-MirrorAvailability -Url $mirror -TimeoutSec $TimeOutSec)
+        {
+            Write-Verbose "$mirror is available "
+            Write-Host "`t $mirror" -ForegroundColor Green
+            # 插入到数组中(这里如果foreach用了-parallel,就会导致无法访问外部的$availableMirros)
+            $availableMirrors += $mirror
+        }
+        else
+        {
+            Write-Verbose "$mirror is not available "
+            Write-Host "`t $mirror " -ForegroundColor Red
+        }
+
+        if ($pscmdlet.ParameterSetName -eq 'First')
+        {
+
+            if (($availableMirrors.Count -ge $First))
+            {
+                break #在foreach-object中会直接停止函数的运行,而使用传统foreach则是正常的
+            }
+        }
+    } 
+    if ($availableMirrors.Count -eq 0)
+    {
+        Write-Warning 'all mirrors are timeout! but there may be some mirrors are available,try to choose one manually...'
+
+        $availableMirrors = $Mirrors
+    }
+    return $availableMirrors
 }
 
 function Test-LinksParallel
