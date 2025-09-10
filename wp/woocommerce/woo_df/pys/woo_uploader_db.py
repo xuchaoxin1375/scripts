7919ba638d3f00aa7502606272d9fe2108265a47
cxxu <838808930@qq.com>
2025-09-10 15:22:14 +0800

    general update project 09/10/2025 15:22:14

--- wp/woocommerce/woo_df/pys/woo_uploader_db.py
+++ wp/woocommerce/woo_df/pys/woo_uploader_db.py
@@ -727,17 +727,31 @@ class WooCommerceProductImporter:
             conn.close()
 
     def update_product_slugs(self):
-        """更新所有产品的 slug，格式为 post_name-ID"""
+        """更新所有产品的 slug，格式为 post_name-ID
+    -- 通常wp数据库限制slug长度200字符(打满不是很好,可能会遇到404),这里先将长度限制到180,然后做后缀id拼接防止重复slug处理(冗余的20位保存id绰绰有余)
+    -- 在产品导入环节中执行的slug规范化处理,长度限制以及id后缀区分同名产品(testing...)
+        
+
+        """
         update_sql = """
-        UPDATE wp_posts AS p
-        JOIN (
-            SELECT ID, post_name, 
-                LEFT(CONCAT(post_name, '-', ID), 200) AS new_slug 
-            FROM wp_posts 
-            WHERE post_type = 'product' AND post_status != 'trash'
-        ) AS sub ON p.ID = sub.ID
-        SET p.post_name = sub.new_slug
-        WHERE p.post_type = 'product';
+            UPDATE wp_posts AS p
+                JOIN (
+                    SELECT ID,
+                        post_name,
+                        CONCAT(
+                            LEFT(
+                                post_name,
+                                180
+                            ),
+                            '-',
+                            ID
+                        ) AS new_slug
+                    FROM wp_posts
+                    WHERE post_type = 'product'
+                        AND post_status != 'trash'
+                ) AS sub ON p.ID = sub.ID
+            SET p.post_name = sub.new_slug
+            WHERE p.post_type = 'product';
         """
         self._execute_sql(update_sql)
 
