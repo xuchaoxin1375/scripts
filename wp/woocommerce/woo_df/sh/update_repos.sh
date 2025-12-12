86a75fafd521fc6fb5f43ab88ff0da7affb6d398
cxxu <838808930@qq.com>
2025-12-12 21:13:35 +0800

    general update project 12/12/2025 21:13:35

--- wp/woocommerce/woo_df/sh/update_repos.sh
+++ wp/woocommerce/woo_df/sh/update_repos.sh
@@ -152,7 +152,9 @@ if [ "$UPDATE_CONFIG" -eq 1 ]; then
         rm  /www/server/nginx/conf/com.conf -fv
     fi
     cp /www/sh/nginx_conf/com.conf /www/server/nginx/conf/com.conf -fv
-    # cp /www/sh/nginx_conf/limit_rate.conf /www/server/nginx/conf/limit_rate.conf -fv
+    cp /www/sh/nginx_conf/com_limit_rate.conf /www/server/nginx/conf/com_limit_rate.conf -fv
+    cp /www/sh/nginx_conf/com_limit_basic.conf /www/server/nginx/conf/com_basic.conf -fv
+    
     cp /www/sh/nginx_conf/nginx.conf /www/server/nginx/conf/nginx.repos.conf -fv
     # todo
     # 如果启用了 --force 选项,则备份宝塔的 nginx.conf 文件 (/www/server/nginx/conf/nginx.conf)
