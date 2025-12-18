79d5c2cb444e302edef4dae6ac27b13a75794b8c
cxxu <838808930@qq.com>
2025-12-18 10:12:58 +0800

    general update project 12/18/2025 10:12:58

--- wp/woocommerce/woo_df/sh/clean_logs.sh
+++ wp/woocommerce/woo_df/sh/clean_logs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # 删除nginx日志(配合crontab使用,比如每2天删除1次)
-rm /www/wwwlogs/*.log
+# rm /www/wwwlogs/*.log
+pattern="/www/wwwlogs/*.log"
+# 将所有*.log日志文件内容置空
+for log in $pattern ; do
+    echo "" > "$log";
+    # 将文件所有者更改为www
+    chown www:www "$log";
+done
