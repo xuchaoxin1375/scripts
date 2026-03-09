42a7b3023959c27ab4dd75842c302dc6bf728436
cxxu <838808930@qq.com>
2026-03-09 23:17:39 +0800

    general update project 03/09/2026 23:17:39

--- wp/woocommerce/woo_df/sh/demo.sh
+++ wp/woocommerce/woo_df/sh/demo.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
+# wait_frist.sh
 log() {
-    echo "[$(date +%T)] $*"
+    echo -e "[$(date +%T)] $*"
 }
-log "将启动一个后台睡眠2秒的作业."
+pids=()
+log "启动5个后台任务"
+sleep 1 &
+pids+=($!)
 sleep 2 &
-pid=$!
+pids+=($!)
+sleep 3 &
+pids+=($!)
+sleep 4 &
+pids+=($!)
+sleep 5 &
+pids+=($!)
+log "$(declare -p pids)"
+log "开始wait -n 第一个任务结束"
+wait -n -p id
+log "\tid1=$id"
 
-log "回到当前shell,此时后台作业[pid=$pid]尚未结束,调用一次jobs看看."
-jobs
-
-log "此时应该提示作业未完成,再调用一次jobs,再次输出和上一次jobs的相同的结果."
-jobs
-
-log "现在等待3秒,确保让作业完成(后台作业虽然已经完成了,但是在结果被读取之前,仍然记录着,\
-随时可以调用jobs观察,但是观察是一次性的,第一次观察后就不能再次观察到了),且结果易于观察."
-sleep 3
-
-log "调用jobs -p 观察,此时仅打印后台任务pid,但是不会显示状态(即便已经结束,也不会被移除)"
-jobs -rp
-
-log "再次调用jobs -p 观察[$pid],仍然可以观察到."
-jobs -rp
-
-log "调用jobs -l 观察[$pid],此时会显示状态,并且会移除该作业"
-jobs -l
-
-log "实验结束[$pid]"
+log "等待4秒,此时至少3个任务结束"
+sleep 4
+log "再次wait -n"
+wait -n -p id
+log "\tid2=$id"
--- wp/woocommerce/woo_df/sh/demo_wait.sh
+++ wp/woocommerce/woo_df/sh/demo_wait.sh
@@ -28,8 +28,24 @@ pids=()
 declare -A pid_task_map # PID -> 任务名 的映射
 failed=0
 failed_tasks=()
+running=0
 # tasks=($(seq 1 "$tasks_count"))
 mapfile -t tasks < <(seq 1 "$tasks_count")
+
+count_status() {
+    pid="$1"
+    exit_code="$2"
+    if [[ $exit_code -ne 0 ]]; then
+        ((failed++))
+        failed_tasks+=("${pid_task_map[$pid]}")
+        status="fail"
+    else
+        ((success++))
+        status="success"
+    fi
+    log "进程 $pid 结束,status=$status"
+}
+
 for task_id in "${tasks[@]}"; do
     # 控制并发数
     while
@@ -38,8 +54,11 @@ for task_id in "${tasks[@]}"; do
     do
         # 等待任意一个子进程结束,轮询策略;(扫描一遍进程ip数组,需要设置每次轮序的时间间隔,看哪个进程结束)
         # log "进程数量[$jobs_count]达到上限,等待任意一个完成"
+        log "当前任务数达到$jobs_count ;逐个释放..."
         wait -n -p pid
-        log "进程 $pid 结束"
+        exit_code=$?
+        count_status "$pid" "$exit_code"
+        # 跳过从pids数组中移除已完成的任务pid,而在外部统一判断处理
     done
 
     run_task "$task_id" &
@@ -48,15 +67,14 @@ for task_id in "${tasks[@]}"; do
     pid_task_map[$pid]="$task_id"
 done
 
-# 等待剩余任务
+log "=====等待剩余任务"
 for pid in "${pids[@]}"; do
-    wait "$pid"
-    exit_code=$?
-    if [[ $exit_code -ne 0 ]]; then
-        ((failed++))
-        failed_tasks+=("${pid_task_map[$pid]}")
+    if kill -p "$pid" 2> /dev/null; then
+        wait "$pid"
+        exit_code=$?
+        count_status "$pid" "$exit_code"
     else
-        ((success++))
+        log "进程$pid 已经结束并统计过"
     fi
 done
 
@@ -64,8 +82,3 @@ log "================================"
 log "Total: ${#tasks[@]}, Failed: $failed"
 log "Success_a: $((${#tasks[@]} - failed)),Success_b: $success"
 log "Failed tasks(ids): ${failed_tasks[*]}"
-
-# log "================================"
-# log "Total failed: $failed"
-# log "Total success: $success"
-# log "Failed tasks: ${failed_tasks[*]}"
