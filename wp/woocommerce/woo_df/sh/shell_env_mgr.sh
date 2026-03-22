1f1c1cc61f95a60a4e737dad7c93518b2c5ee45a
cxxu <838808930@qq.com>
2026-03-22 12:40:47 +0800

    general update project 03/22/2026 12:40:47

--- wp/woocommerce/woo_df/sh/shell_env_mgr.sh
+++ wp/woocommerce/woo_df/sh/shell_env_mgr.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC1091
 # 在这里可以手动维护一些常用的环境管理的程序的导入脚本,注意检查相关环境变量,避免重复导入;
-if [[ -z $NVM_DIR ]]; then
+echo "Loading common env mgr..."
+# DON'T EXPORT *LOADED ENV VARIABLES
+_NVM_LOADED=""
+if [[ -z $_NVM_LOADED ]]; then
+    echo "Loading nvm..."
+    _NVM_LOADED="1"
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
