974216fc5f5c5715e43e680165874b74dbff4526
cxxu <838808930@qq.com>
2026-04-15 19:04:12 +0800

    add update repos alias for zsh

--- wp/woocommerce/woo_df/sh/macos_sh/shell_macos.sh
+++ wp/woocommerce/woo_df/sh/macos_sh/shell_macos.sh
@@ -3,3 +3,4 @@ echo "Loading env for macos..."
 
 alias typora="open -a /Applications/Typora.app"
 alias push_ReposesConfiged="pwsh -c '& {Push-ReposesConfiged ; pause}'"
+alias update_ReposesConfiged="pwsh -c '& {Update-ReposesConfiged}'"
