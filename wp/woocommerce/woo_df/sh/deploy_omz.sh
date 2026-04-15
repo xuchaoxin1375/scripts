725ed9d7dbdec39b47ab0c42391053f3f1651d69
cxxu <838808930@qq.com>
2026-04-16 00:56:34 +0800

    general update project 04/16/2026 00:56:34

--- wp/woocommerce/woo_df/sh/deploy_omz.sh
+++ wp/woocommerce/woo_df/sh/deploy_omz.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 # 配置zsh(oh my zsh)相关插件,让体验向fish靠拢
 # 此部署代码尽量实现幂等性(避免反复运行导致配置文件内容混乱.)
-echo "version:20260411"
+# 补全类插件要求比较严格,尤其是动态自动补全实现比较复杂需要更多的步骤
+# 测试补全插件效果时,对于基础工具,注意区分gnu版本和bsd版本,可用选项和风格有所不同
+version=20260415
+install_zsh_completions=false
+install_zsh_autocomplete=true
+usage='
+version:'"$version"'
+usage:
+    deploy_omz.sh [options]
+options:
+    -zc | --install-zsh-completions [true|false]
+        install zsh-completions plugin if true
+    -zac | --install-zsh-autocomplete [true|false]
+        install zsh-autocomplete plugin if true
+'
+parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -zc | --install-zsh-completions)
+                install_zsh_completions="$2"
+                shift
+                ;;
+            -zac | --install-zsh-autocomplete)
+                install_zsh_autocomplete="$2"
+                shift
+                ;;
+            -h | --help)
+                echo "$usage"
+                exit 0
+                ;;
+            -*)
+                echo "unknown option: "
+                echo "$usage"
+                exit 1
+                ;;
+        esac
+        shift
+    done
+}
 # 以下代码需要gnu sed,如果gsed不可用,请用户安装
 if [[ $OSTYPE == darwin* ]]; then
     if command -v gsed &> /dev/null; then
@@ -23,6 +61,7 @@ zshp=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 zhssp=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 zcp=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 zysu=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+zac=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 ! [[ -d $zap ]] && git clone --depth 1 https://gitee.com/zsh-users/zsh-autosuggestions $zap
 
 ! [[ -d $zshp ]] && git clone --depth 1 https://gitee.com/zsh-users/zsh-syntax-highlighting.git $zshp
@@ -32,7 +71,10 @@ zysu=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 # zsh-completions这个项目gitee官方可能没有镜像,使用个人用户的自镜像版本(建议有需要的可以自己拉取一份比较安全)
 # 另外这个插件比其他zsh插件不同,在配合oh my zsh使用时需要额外注意配置文件的写法;
 ! [[ -d $zcp ]] && git clone --depth 1 https://gitee.com/duchenpaul/zsh-completions.git $zcp
-! [[ -d $zysu ]] && git clone https://gitcode.com/gh_mirrors/zs/zsh-you-should-use.git $zysu
+! [[ -d $zysu ]] && git clone --depth 1 https://gitcode.com/gh_mirrors/zs/zsh-you-should-use.git $zysu
+# 自动动态的补全预测,属于较复杂插件(代替incr.zsh)
+! [[ -d $zac ]] && git clone --depth 1 https://gitee.com/mirrors/zsh-autocomplete.git $zac
+
 # 将工作目录转移到家目录
 # cd ~ || exit 1
 zshrc_path="$HOME/.zshrc"
@@ -42,6 +84,7 @@ plugins_list=$(
     cat << EOF
     git
     z
+    zsh-autocomplete
     zsh-syntax-highlighting
     zsh-autosuggestions
     zsh-history-substring-search
@@ -58,37 +101,53 @@ if grep '^plugins=(.*)' "$zshrc_path"; then
     sed -i 's/^plugins=(.*)/\
 plugins=(\
 '"$plugins_list"'
-)/' -i "$zshrc_path"
+)/' "$zshrc_path"
 elif grep '^plugins=($' "$zshrc_path"; then
     echo "覆盖插件列表(多行plugins更新)"
     sed -i '/^plugins=(/,/)/c\
 plugins=(\
 '"$plugins_list"'
-)' -i "$zshrc_path"
+)' "$zshrc_path"
 
 fi
+echo "checking and install completion plugins..."
 # 在plugins= 行或者source $ZSH/oh-my-zsh.sh行上方插入额外片段行(适用于zsh-completions)
 # shellcheck disable=SC2016
 # sed -i '/source \$ZSH\/oh-my-zsh\.sh/i\
-if ! grep '# >>> zsh-completions' "$zshrc_path"; then
-    sed -i '/^plugins=(/i\
+if [[ "$install_zsh_completions" == "true" ]]; then
+    echo "安装zsh-completions ..."
+    if ! grep '# >>> zsh-completions' "$zshrc_path"; then
+        sed -i '/^plugins=(/i\
 # >>> zsh-completions\
 fpath+=${ZSH_CUSTOM:-${ZSH:-~\/.oh-my-zsh}\/custom}\/plugins\/zsh-completions\/src\
 autoload -U compinit \&\& compinit\
 # <<< zsh-completions\
 ' ~/.zshrc
+    fi
+    # 重建补全(词库)
+    rm -f ~/.zcompdump
 fi
-# 重建补全
-rm -f ~/.zcompdump
-
-#检查配置文件是否有相应的行
-cat "$zshrc_path" | grep -e zsh-syntax-highlighting -e zsh-autosuggestions -e zsh-history-substring-search -e zsh-completions
+# 向.zshrc文件头部插入source命令
+if [[ "$install_zsh_autocomplete" == "true" ]]; then
+    echo "安装zsh-autocomplete ..."
+    if ! grep '# >>> zsh-autocomplete' "$zshrc_path"; then
+        # shellcheck disable=SC2016
+        sed -i '1i\
+# >>> zsh-autocomplete\
+# source /path/to/zsh-autocomplete/zsh-autocomplete.plugin.zsh\
+source ${ZSH_CUSTOM:-~\/.oh-my-zsh\/custom}\/plugins\/zsh-autocomplete\/zsh-autocomplete.plugin.zsh\
+# <<< zsh-autocomplete\
+' ~/.zshrc
+    fi
+fi
 
 # 利用sed并启用扩展正则原地修改,将Zsh主题设置为随机
 sed -Ei 's/(^ZSH_THEME=)(.*)/\1"random"/' "$zshrc_path"
 #设置随机选择的主题的列表为ys,junkfood,rkj-repos;具体可以改成自己喜欢的主题
 sed -Ei.bak 's/(^#*\s*)(ZSH_THEME_RANDOM.*=)(.*)/\2("ys" "junkfood" )/' "$zshrc_path"
 #检查修改结果
+#检查配置文件是否有相应的行
+cat "$zshrc_path" | grep -e zsh-syntax-highlighting -e zsh-autosuggestions -e zsh-history-substring-search -e zsh-autocomplete -e zsh-completions
 cat ~/.zshrc | grep -E '^[^#]' | grep -e random -e THEME -e RANDOM | cat -n
 #刷新配置结果
 # shellcheck disable=SC1090
