e68b5d547a35788c39144f566cdf0adf622eaae2
cxxu <838808930@qq.com>
2026-05-28 21:45:55 +0800

    general update project 05/28/2026 21:45:55

--- wp/woocommerce/woo_df/sh/clean_bad_pkgs.sh
+++ wp/woocommerce/woo_df/sh/clean_bad_pkgs.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # 根据包名(部分名)清理不在需要的包文件,例如网站压缩包
 FIND_BASE="/www/wwwroot/xcx/" #可以自行酌情增加精确度,比如添加服务器名到路径中
+white_list_file=""
 # 参数解析
 args_pos=()
 parse_args() {
@@ -15,9 +16,13 @@ parse_args() {
                 echo "$usage"
                 exit 0
                 ;;
+            -f | --file)
+                white_list_file="$2"
+                shift
+                ;;
             --)
                 shift
-                break 
+                break
                 ;;
             -?*)
                 echo "Unknown option: " >&2 #输出错误信息到标准错误
@@ -34,83 +39,20 @@ parse_args() {
 }
 parse_args "$@"
 set -- "${args_pos[@]}"
-
-FIND_BASE="${1:-$FIND_BASE}" #如果用户提供了参数则覆盖默认值
-file_names=(
-    thepethutco.com
-    useffortlessmart.com
-    deindealdorf.com
-    cdandscore.com
-    wohnmobildeal.com
-    germanlifeparts.com
-    homepartssolutions.com
-    livingaccessoriesde.com
-    wisehomeshopping.com
-    parislivingshop.com
-    usacarpartsexpress.com
-    essentiel-hab.com
-    ukpartplace.com
-    valuehub24.com
-    autoteileplus.com
-    shoplabgear.com
-    foodtrailus.com
-    libropulsor.com
-    plantacionhogar.com
-    german-visionshop.com
-    occhialiday.com
-    oggitutto.com
-    germabeauty.com
-    derweinladende.com
-    highstreetvault.com
-    bladehubus.com
-    mesapantry.com
-    faitletemps.com
-    ateliermaisonfr.com
-    klugbuch.com
-    leselotse.com
-    diversionshop.com
-    grabmartpro.com
-    toutdedans.com
-    sietediasfull.com
-    milcosasbuenas.com
-    ukplugpro.com
-    geschichtenhafen.com
-    papierwald.com
-    snugbuyuk.com
-    laboverte.com
-    bearingrevolution-uk.com
-    calidadprecioya.com
-    todoparatiya.com
-    plazageneral.com
-    chassfolk.com
-    palabraytinta.com
-    rinconletras.com
-    bookauralive.com
-    britstorybox.com
-    isleofreads.com
-    londonleafs.com
-    panierconfiant.com
-    quietchapteres.com
-    ukbookharbor.com
-    monexpert-maison.com
-    dolceguida.com
-    homedepotly.com
-    frtirshop.com
-    shop365tir.com
-    joyloompro.com
-    librosenvivo.com
-    elektroplatt.com
-    easyfrancedaily.com
-    energiefra.com
-    buenoshogares.com
-    bouquinpassion.com
-    ukstudiosupply.com
-    homemartfr.com
+declare -a names=(
 )
+if [[ -f "$white_list_file" ]]; then
+    echo "从名单文件 '$white_list_file' 中包名..."
+    nl "$white_list_file"
+    mapfile -t names < <(tr -d '\r' < "$white_list_file")
+else
+    echo "警告: 白名单文件 '$white_list_file' 不存在或不可读, 将使用默认的包名列表." >&2
+fi
+FIND_BASE="${1:-$FIND_BASE}" #如果用户提供了参数则覆盖默认值
 # 检查白名单数组
-# declare -p file_names
+# declare -p names
 
-for name in "${file_names[@]}"; do
+for name in "${names[@]}"; do
     echo "正在尝试清理包含 '$name' 的包文件..."
     # find $FIND_BASE -type f -name "$name.*"  # -exec rm -f {} \;
     find "$FIND_BASE" -type f -regextype posix-extended -regex ".*/$name(\..*)?\.(zip|zst|lz4|7z|gz|gzip)" -exec rm -fv {} +
