14aa18d604ec371e8336b4dc048c51b16e086413
cxxu <838808930@qq.com>
2026-06-04 17:21:51 +0800

    general update project 06/04/2026 17:21:51

---
+++ wp/woocommerce/woo_df/sh/clean_reverse_server_logs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# 使用truncate清空(指定文件)日志文件内容,会直接把文件大小变成 0，通常是最省心、最稳定的做法。
+# 对于持续写入中的 Nginx access/error 日志，这也是更合适的清理方式。
+dirs=(
+    /www/wwwlogs
+    /var/log/nginx
+)
+
+for logdir in "${dirs[@]}"; do
+    [ -d "$logdir" ] &&
+        find "$logdir" \
+            -type f \
+            \(-name "*access.log" -o -name "*error.log" \) \
+            -exec truncate -s 0 {} +
+
+done
